465815932811e88bc2dfca165b751c6fe99895e5
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2024-12-13 22:20:23 +0800

    Update statcalc.js

--- statcalc.js
+++ statcalc.js
@@ -15,29 +15,8 @@ function getEfficiency (level) {
   return efficiency;
 }
 
-function calculateEnhancement(level, selectedEnhancement) {
-  let calculatedStat = level * 10; // Basic calculation for level
-
-  // Modify stat based on the selected enhancement type
-  switch (selectedEnhancement) {
-    case 'Lucky':
-      calculatedStat += 5;
-      break;
-    case 'Fighter':
-      calculatedStat += 10;
-      break;
-    case 'Wizard':
-      calculatedStat += 7;
-      break;
-    case 'Healer':
-      calculatedStat += 3;
-      break;
-    case 'Thief':
-      calculatedStat += 8;
-      break;
-    default:
-      break;
-  }
-
-  return calculatedStat;
+function getTotalStats (level) {
+  let totalStats = 15+747*(level-1)/(100-1);
+  return totalStats;
 }
+  
