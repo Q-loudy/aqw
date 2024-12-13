980620c38daec83847c1841a745f8d074caf0927
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2024-12-13 20:43:14 +0800

    Create statcalc.js

---
+++ statcalc.js
@@ -0,0 +1,28 @@
+// enhancement.js
+
+function calculateEnhancement(level, selectedEnhancement) {
+  let calculatedStat = level * 10; // Basic calculation for level
+
+  // Modify stat based on the selected enhancement type
+  switch (selectedEnhancement) {
+    case 'Lucky':
+      calculatedStat += 5;
+      break;
+    case 'Fighter':
+      calculatedStat += 10;
+      break;
+    case 'Wizard':
+      calculatedStat += 7;
+      break;
+    case 'Healer':
+      calculatedStat += 3;
+      break;
+    case 'Thief':
+      calculatedStat += 8;
+      break;
+    default:
+      break;
+  }
+
+  return calculatedStat;
+}
