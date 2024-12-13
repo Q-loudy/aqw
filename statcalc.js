2a5b52c6e9fe786ec54c1270a8d93f6114536190
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2024-12-13 23:18:39 +0800

    base primary stats

--- statcalc.js
+++ statcalc.js
@@ -34,75 +34,75 @@ function getBasePrimaryStats(level, classModel) {
 
   switch (classModel) {
     case 'Tank Melee':
-        strength += baseStats * 0.27;
-        intellect += baseStats * 0.05;
-        endurance += baseStats * 0.30;
-        dexterity += baseStats * 0.22;
-        wisdom += baseStats * 0.10;
-        luck += baseStats * 0.06;
+        basePrimaryStats.strength += baseStats * 0.27;
+        basePrimaryStats.intellect += baseStats * 0.05;
+        basePrimaryStats.endurance += baseStats * 0.30;
+        basePrimaryStats.dexterity += baseStats * 0.22;
+        basePrimaryStats.wisdom += baseStats * 0.10;
+        basePrimaryStats.luck += baseStats * 0.06;
         break;
     
     case 'Dodge Melee':
-        strength += baseStats * 0.20;
-        intellect += baseStats * 0.05;
-        endurance += baseStats * 0.22;
-        dexterity += baseStats * 0.33;
-        wisdom += baseStats * 0.10;
-        luck += baseStats * 0.10;
+        basePrimaryStats.strength += baseStats * 0.20;
+        basePrimaryStats.intellect += baseStats * 0.05;
+        basePrimaryStats.endurance += baseStats * 0.22;
+        basePrimaryStats.dexterity += baseStats * 0.33;
+        basePrimaryStats.wisdom += baseStats * 0.10;
+        basePrimaryStats.luck += baseStats * 0.10;
         break;
 
     case 'Power Melee':
-        strength += baseStats * 0.30;
-        intellect += baseStats * 0.02;
-        endurance += baseStats * 0.18;
-        dexterity += baseStats * 0.30;
-        wisdom += baseStats * 0.06;
-        luck += baseStats * 0.14;
+        basePrimaryStats.strength += baseStats * 0.30;
+        basePrimaryStats.intellect += baseStats * 0.02;
+        basePrimaryStats.endurance += baseStats * 0.18;
+        basePrimaryStats.dexterity += baseStats * 0.30;
+        basePrimaryStats.wisdom += baseStats * 0.06;
+        basePrimaryStats.luck += baseStats * 0.14;
         break;
 
     case 'Offensive Caster':
-        strength += baseStats * 0.06;
-        intellect += baseStats * 0.33;
-        endurance += baseStats * 0.20;
-        dexterity += baseStats * 0.11;
-        wisdom += baseStats * 0.15;
-        luck += baseStats * 0.15;
+        basePrimaryStats.strength += baseStats * 0.06;
+        basePrimaryStats.intellect += baseStats * 0.33;
+        basePrimaryStats.endurance += baseStats * 0.20;
+        basePrimaryStats.dexterity += baseStats * 0.11;
+        basePrimaryStats.wisdom += baseStats * 0.15;
+        basePrimaryStats.luck += baseStats * 0.15;
         break;
 
     case 'Defensive Caster':
-        strength += baseStats * 0.08;
-        intellect += baseStats * 0.30;
-        endurance += baseStats * 0.27;
-        dexterity += baseStats * 0.10;
-        wisdom += baseStats * 0.10;
-        luck += baseStats * 0.15;
+        basePrimaryStats.strength += baseStats * 0.08;
+        basePrimaryStats.intellect += baseStats * 0.30;
+        basePrimaryStats.endurance += baseStats * 0.27;
+        basePrimaryStats.dexterity += baseStats * 0.10;
+        basePrimaryStats.wisdom += baseStats * 0.10;
+        basePrimaryStats.luck += baseStats * 0.15;
         break;
 
     case 'Power Caster':
-        strength += baseStats * 0.06;
-        intellect += baseStats * 0.28;
-        endurance += baseStats * 0.23;
-        dexterity += baseStats * 0.05;
-        wisdom += baseStats * 0.28;
-        luck += baseStats * 0.10;
+        basePrimaryStats.strength += baseStats * 0.06;
+        basePrimaryStats.intellect += baseStats * 0.28;
+        basePrimaryStats.endurance += baseStats * 0.23;
+        basePrimaryStats.dexterity += baseStats * 0.05;
+        basePrimaryStats.wisdom += baseStats * 0.28;
+        basePrimaryStats.luck += baseStats * 0.10;
         break;
 
     case 'Full Hybrid':
-        strength += baseStats * 0.24;
-        intellect += baseStats * 0.24;
-        endurance += baseStats * 0.20;
-        dexterity += baseStats * 0.20;
-        wisdom += baseStats * 0.07;
-        luck += baseStats * 0.05;
+        basePrimaryStats.strength += baseStats * 0.24;
+        basePrimaryStats.intellect += baseStats * 0.24;
+        basePrimaryStats.endurance += baseStats * 0.20;
+        basePrimaryStats.dexterity += baseStats * 0.20;
+        basePrimaryStats.wisdom += baseStats * 0.07;
+        basePrimaryStats.luck += baseStats * 0.05;
         break;
 
-    case 'Luck Hybrid':
-        strength += baseStats * 0.22;
-        intellect += baseStats * 0.08;
-        endurance += baseStats * 0.18;
-        dexterity += baseStats * 0.21;
-        wisdom += baseStats * 0.08;
-        luck += baseStats * 0.23;
+    case 'basePrimaryStats.luck Hybrid':
+        basePrimaryStats.strength += baseStats * 0.22;
+        basePrimaryStats.intellect += baseStats * 0.08;
+        basePrimaryStats.endurance += baseStats * 0.18;
+        basePrimaryStats.dexterity += baseStats * 0.21;
+        basePrimaryStats.wisdom += baseStats * 0.08;
+        basePrimaryStats.luck += baseStats * 0.23;
         break;
 
     default:
