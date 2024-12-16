673e05089ba40b21c9942e2903a3cc23709d60d9
Qloudy <117589336+Q-loudy@users.noreply.github.com>
2024-12-16 22:03:43 +0800

    Update statcalc.js

--- statcalc.js
+++ statcalc.js
@@ -385,13 +385,13 @@ function getSecondaryStats(level, classModel, primaryStats) {
   efficiency = getEfficiency(level);
   
   secondaryStats.health = baseHP + secondaryStats.health;
-  secondaryStats.hitChance = Math.round(90 + secondaryStats.hitChance * efficiency, 2);
-  secondaryStats.haste = Math.round(secondaryStats.haste * efficiency, 2);
-  secondaryStats.critChance = Math.round(5 + secondaryStats.critChance * efficiency, 2);
-  secondaryStats.critMod = Math.round(150 + secondaryStats.critMod * efficiency, 2);
-  secondaryStats.evasion = Math.round(4 + secondaryStats.evasion * efficiency, 2);
-  secondaryStats.magIn = Math.round(secondaryStats.magIn * efficiency, 2);
-  secondaryStats.magOut = Math.round(secondaryStats.magOut * efficiency, 2);
+  secondaryStats.hitChance = Math.round(90 + secondaryStats.hitChance * efficiency * 100) / 100;
+  secondaryStats.haste = Math.round(secondaryStats.haste * efficiency * 100) / 100;
+  secondaryStats.critChance = Math.round(5 + secondaryStats.critChance * efficiency * 100) / 100;
+  secondaryStats.critMod = Math.round(150 + secondaryStats.critMod * efficiency * 100) / 100;
+  secondaryStats.evasion = Math.round(4 + secondaryStats.evasion * efficiency * 100) / 100;
+  secondaryStats.magIn = Math.round(secondaryStats.magIn * efficiency * 100) / 100;
+  secondaryStats.magOut = Math.round(secondaryStats.magOut * efficiency * 100) / 100;
 
   return secondaryStats;
 }
